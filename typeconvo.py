4e2353742d5e13e5b815e9b49dd57dc94501337a
mounika1017 <24331a05j7@mvgrce.edu.in>
2025-12-12 16:44:43 +0530

    Add files via upload

---
+++ typeconvo.py
@@ -0,0 +1,10 @@
+num=float(input("enter a number:"))
+print("value is:",num)
+print("type:",type(num))
+num1=int(num)
+print("value is:",num1)
+print("type :",type(num1))
+a="55"
+print("type conversion rom string to int:",int(a))
+b="5.64"
+print("type conversion from string to float:",float(b))
