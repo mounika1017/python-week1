df1be93eef57b13744d303b11546ee86d5afc862
mounika1017 <24331a05j7@mvgrce.edu.in>
2025-12-12 16:47:37 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,11 @@
+import math
+print("value of pi",math.pi)
+r=float(input("enter the r value:"))
+print("area of circle:",math.pi*r**2)
+print("circumference of a circle:",2*math.pi*r)
+print("square root of a number:",math.sqrt(49))
+print("square root of another number:",math.sqrt(226))
+print("cos 45 value:",math.cos(45))
+print("sin 45 value:",math.sin(45))
+
+
